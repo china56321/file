1c4e607acda9361013b2b0e61a7fdac129e60a7e
china56321 <42080579+china56321@users.noreply.github.com>
2019-12-12 16:47:13 +0800

    Create rename_img.py

---
+++ rename_img.py
@@ -0,0 +1,33 @@
+# -*- coding:utf8 -*-
+#!/usr/bin/python2.7
+import os
+
+class BatchRename():
+    '''
+    批量重命名文件夹中的图片文件
+
+    '''
+    def __init__(self):
+        self.path = '/home/lifan/share/ML/160115000'  #路径 
+
+    def rename(self):
+        filelist = os.listdir(self.path)
+        total_num = len(filelist)
+        i = 24258 #图片名称起始值
+
+        for item in filelist:
+            if item.endswith('.jpg'):
+                src = os.path.join(os.path.abspath(self.path), item)
+                str1=str(i)
+                dst = os.path.join(os.path.abspath(self.path), str1.zfill(6) + '.jpg') #图片名称为5位数，即第一张图片为000000，第二张图片为000001，以此类推.......
+                try:
+                    os.rename(src, dst)
+                    print ('converting %s to %s ...' % (src, dst))
+                    i = i + 1
+                except:
+                    continue
+       # print 'total %d to rename & converted %d jpgs' % (total_num, i)
+
+if __name__ == '__main__':
+    demo = BatchRename()
+    demo.rename()
