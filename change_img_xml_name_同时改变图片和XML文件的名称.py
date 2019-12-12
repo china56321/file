0efc26b863ea23d98993fed0b3140bf0c3cfd560
china56321 <42080579+china56321@users.noreply.github.com>
2019-12-12 16:36:01 +0800

    Create change_img_xml_name_同时改变图片和XML文件的名称.py
    同时改变图片和XML文件的名称.

---
+++ change_img_xml_name_同时改变图片和XML文件的名称.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from PIL import Image
+import os.path
+import glob
+import xml.etree.ElementTree as ET
+import xml.dom.minidom
+ 
+i = 0
+xmldir = "/home/lifan/share/delete/Annotations"
+imgsdir = "/home/lifan/share/delete/JPEGImages"
+outdir = "/home/lifan/share/delete/save"
+for xmlfile in os.listdir(xmldir):
+    xmlname = os.path.splitext(xmlfile)[0]
+    for pngfile in os.listdir(imgsdir):
+        pngname = os.path.splitext(pngfile)[0]
+        if pngname == xmlname:
+             # 修改图片文件名
+             # 图片文件名修改前后的路径
+             olddir = os.path.join(os.path.abspath(imgsdir), pngname + ".jpg")
+             newdir = os.path.join(os.path.abspath(imgsdir), str(i)+".jpg")
+             os.rename(olddir, newdir)
+             print(xmlfile, '----->', str(i) + '.jpg')
+             # 修改filename结点属性
+             # 读取xml文件
+             dom = xml.dom.minidom.parse(os.path.join(xmldir, xmlfile))
+             root = dom.documentElement
+ 
+             # 获取标签对filename之间的值并赋予新值i
+             root.getElementsByTagName('filename')[0].firstChild.data = str(i) + '.jpg'
+ 
+             # 将修改后的xml文件保存
+             # xml文件修改前后的路径
+             old_xmldir = os.path.join(xmldir, xmlfile)
+             new_xmldir = os.path.join(xmldir, str(i)+'.xml')
+             # 打开并写入
+             with open(old_xmldir, 'w') as fh:
+                 dom.writexml(fh)
+             os.rename(old_xmldir, new_xmldir)
+             i += 1
+print('total number is ', i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
