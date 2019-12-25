4fccf2188af1cbd681c989610ad5fb9977e18f45
china56321 <42080579+china56321@users.noreply.github.com>
2019-12-25 10:54:51 +0800

    Add files via upload
    png转为 jpg格式

---
+++ png2jpyg.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import cv2 as cv
+import os
+
+def PNG_JPG(PngPath):
+    img = cv.imread(PngPath, 0)
+    w, h = img.shape[::-1]
+    infile = PngPath
+    outfile = os.path.splitext(infile)[0] + ".jpg"
+    img = Image.open(infile)
+    img = img.resize((int(w), int(h)), Image.ANTIALIAS)
+    try:
+        if len(img.split()) == 4:
+            # prevent IOError: cannot write mode RGBA as BMP
+            r, g, b, a = img.split()
+            img = Image.merge("RGB", (r, g, b))
+            img.convert('RGB').save(outfile, quality=70)
+            os.remove(PngPath)
+        else:
+            img.convert('RGB').save(outfile, quality=70)
+            os.remove(PngPath)
+        return outfile
+    except Exception as e:
+        print("PNG转换JPG 错误", e)
+
+PNG = "/home/lifan/file/file/datasets/kitti/data_object_image_2/test/"
+f = os.listdir(PNG)
+n = 0
+for i in f:
+    oldname = PNG + f[n]
+    print(oldname)
+    PNG_JPG(oldname)
+    n += 1
+
+
+
