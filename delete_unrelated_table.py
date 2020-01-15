34dfaea4ac4f8e82dca2209f678e9971c56e70da
china56321 <42080579+china56321@users.noreply.github.com>
2020-01-15 17:03:02 +0800

    Update delete_unrelated_table.py

--- delete_unrelated_table.py
+++ delete_unrelated_table.py
@@ -38,7 +38,7 @@ def batch_xmlCompress(old_dirPath, new_dirPath):
 
 # 主函数    
 if __name__ == '__main__':
-    batch_xmlCompress('/home/lifan/share/make/RefineDet/data/VOCdevkit/VOC2007/Annotations/', '/home/lifan/share/make/RefineDet/data/VOCdevkit/VOC2007/tested/')
+    batch_xmlCompress('/home/share/make/RefineDet/data/VOCdevkit/VOC2007/Annotations/', '/home/share/make/RefineDet/data/VOCdevkit/VOC2007/tested/')
     print('all xml files have been compressed')
 
 
