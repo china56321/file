6f5712d2456f183897d29907bc708643874d069d
china56321 <42080579+china56321@users.noreply.github.com>
2020-01-15 17:00:31 +0800

    Update choose_file_选择子文件夹下的文件到另一文件夹下.py

--- choose_file_选择子文件夹下的文件到另一文件夹下.py
+++ choose_file_选择子文件夹下的文件到另一文件夹下.py
@@ -1,6 +1,6 @@
 import shutil,os
-new_path='/home/lifan/share/ML/video/image_2019-9-24/xml'
-for derName, subfolders, filenames in os.walk('/home/lifan/share/ML/video/image_2019-9-24/label'):
+new_path='/home/share/ML/video/image_2019-9-24/xml'
+for derName, subfolders, filenames in os.walk('/home/share/ML/video/image_2019-9-24/label'):
     print(derName)
     print(subfolders)
     print(filenames)
