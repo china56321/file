7f8f7655700903130759226059da555812459e7e
china56321 <42080579+china56321@users.noreply.github.com>
2020-01-15 17:01:12 +0800

    Update choose_img_random_according_to_xml.py

--- choose_img_random_according_to_xml.py
+++ choose_img_random_according_to_xml.py
@@ -5,9 +5,9 @@ import os
 #import os.path
 import shutil  #Python文件复制相应模块
  
-label_dir='/home/lifan/share/make/RefineDet/data/VOCdevkit/VOC2007_bdd100k_train/JPEGImages/'  #所有图片文件所在文件夹
-annotion_dir='/home/lifan/share/make/RefineDet/data/VOCdevkit/VOC2007_bdd100k_train/truck_img/'  #粘贴对应图片名称的图片文件到指定文件夹
-path = '/home/lifan/share/make/RefineDet/data/VOCdevkit/VOC2007_bdd100k_train/truck/'   #xml文件夹
+label_dir='/home/share/make/RefineDet/data/VOCdevkit/VOC2007_bdd100k_train/JPEGImages/'  #所有图片文件所在文件夹
+annotion_dir='/home/share/make/RefineDet/data/VOCdevkit/VOC2007_bdd100k_train/truck_img/'  #粘贴对应图片名称的图片文件到指定文件夹
+path = '/home/share/make/RefineDet/data/VOCdevkit/VOC2007_bdd100k_train/truck/'   #xml文件夹
 path_list = os.listdir(path)# os.listdir(file)会历遍文件夹内的文件并返回一个列表
 path_name=[]  # 定义一个空列表,不需要path_list中的后缀名
 # 利用循环历遍path_list列表并且利用split去掉后缀名
