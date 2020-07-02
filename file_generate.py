7caf69a830ba6102988dacac31b0944dbe6e423d
china56321 <42080579+china56321@users.noreply.github.com>
2020-07-02 15:18:08 +0800

    Update file_generate.py

--- file_generate.py
+++ file_generate.py
@@ -4,7 +4,7 @@ import os
 
 def mkd():
 	k = 0000 # 两位数,不足向前取零
-	path = "/home/lifan/share/ML/" #文件存哪里
+	path = "/home/share/ML/" #文件存哪里
 	for i in range(1,8): #创建一个文件并循环20-1次
 		file_name = str(k) #给文件命名 路径+ 文件标号（以标号为名字）
 		os.makedirs(file_name) #创建文件
