471cecc57b090285194ca0754cc3fd01d017e28a
china56321 <42080579+china56321@users.noreply.github.com>
2020-07-23 16:42:46 +0800

    Add files via upload

---
+++ autoyolov5.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+
+
+
+'''' 
+This sript is used to labeling the images and save the detected image in the output folder.
+
+Just need two steps .
+
+First ,you need to annotate plot_one_box() fucntion in "yolov5/utils/utils.py" on line 913.
+
+Second ,Change the path of image_file to be annotated.
+
+'''
+
+
+import argparse
+import os 
+import torch.backends.cudnn as cudnn
+
+from utils import google_utils
+from utils.datasets import *
+from utils.utils import *
+
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))           
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label: 
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+
+        xmin=int(x[0])
+        ymin=int(x[1])
+        xmax=int(x[2])
+        ymax=int(x[3])
+
+        xml_file.write('    <object>\n')
+        xml_file.write('        <name>' + str(label.split(" ")[0]) + '</name>\n')
+        xml_file.write('        <pose>Unspecified</pose>\n')
+        xml_file.write('        <truncated>0</truncated>\n')
+        xml_file.write('        <difficult>0</difficult>\n')
+        xml_file.write('        <bndbox>\n')
+        xml_file.write('            <xmin>' + str(xmin) + '</xmin>\n')
+        xml_file.write('            <ymin>' + str(ymin) + '</ymin>\n')
+        xml_file.write('            <xmax>' + str(xmax) + '</xmax>\n')
+        xml_file.write('            <ymax>' + str(ymax) + '</ymax>\n')
+        xml_file.write('        </bndbox>\n')
+        xml_file.write('    </object>\n')
+       
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+   
+def detect(save_img=False):
+    out, source, weights, view_img, save_txt, imgsz = \
+        opt.output,opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size  
+    webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
+
+    # Initialize
+    device = torch_utils.select_device(opt.device)
+    # if os.path.exists(out):
+    #     shutil.rmtree(out)  # delete output folder
+    # os.makedirs(out)  # make new output folder
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    google_utils.attempt_download(weights)
+    model = torch.load(weights, map_location=device)['model'].float()  # load to FP32
+    # torch.save(torch.load(weights, map_location=device), weights)  # update model if SourceChangeWarning
+    # model.fuse()
+    model.to(device).eval()
+    imgsz = check_img_size(imgsz, s=model.model[-1].stride.max())  # check img_size
+    if half:
+        model.half()  # to FP16
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = torch_utils.load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model'])  # load weights
+        modelc.to(device).eval()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = True
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz)
+    else:
+        save_img = True
+        dataset = LoadImages(source, img_size=imgsz)
+
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+   
+    # Run inference
+    t0 = time.time()
+    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+    _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+    a=0
+    for path, img, im0s, vid_cap in dataset:
+        path='/home/ambavm/make/yolov5/'+path   # The path of the images needed to be annotated.
+        image = cv2.imread(path) 
+        img_size = image.shape
+        height=img_size[0]
+        width=img_size[1]
+
+        # Create head of  the XML file
+
+        xml_name=path.split(".")[0]
+        print("xml_name:",xml_name)
+        global xml_file
+        xml_file = open((xml_name + '.xml'), 'w')
+        xml_file.write('<?xml version="1.0" ?>\n')
+        xml_file.write('<annotation>\n')
+        xml_file.write('    <folder>VOC2007</folder>\n')
+        xml_file.write('    <filename>' + xml_name + '.jpg'+ '</filename>\n')
+        xml_file.write('    <source>\n')
+        xml_file.write('        <database>The AUTO Database</database>\n')
+        xml_file.write('        <annotation>AUTO by fan</annotation>\n')
+        xml_file.write('        <image>flickr</image>\n')
+        xml_file.write('    </source>\n')
+        xml_file.write('    <size>\n')
+        xml_file.write('        <width>' + str(width) + '</width>\n')
+        xml_file.write('        <height>' + str(height) + '</height>\n')
+        xml_file.write('        <depth>3</depth>\n')
+        xml_file.write('    </size>\n')
+        
+        #end
+
+        # if path == image_file:                  # if name of  path is equal to the name of the path of image_file .
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+           img = img.unsqueeze(0)
+
+        # Inference
+        t1 = torch_utils.time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t2 = torch_utils.time_synchronized()
+        
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+            else:
+                p, s, im0 = path, '', im0s
+
+            save_path = str(Path(out) / Path(p).name)
+            txt_path = str(Path(out) / Path(p).stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if det is not None and len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in det:
+                    if save_txt:  # Write to file 
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
+
+                    if save_img or view_img:  # Add bbox to image
+                        label = '%s %.2f' % (names[int(cls)], conf)
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
+
+                 
+                    # Print time (inference + NMS)
+                    print('%sDone. (%.3fs)' % (s, t2 - t1))
+                    # Stream results
+                    if view_img:
+                        cv2.imshow(p, im0)
+                        if cv2.waitKey(1) == ord('q'):  # q to quit
+                            raise StopIteration
+    
+                    # Save results (image with detections)
+                    if save_img:
+                        if dataset.mode == 'images':
+                            cv2.imwrite(save_path, im0)
+
+                        else:
+                            if vid_path != save_path:  # new video
+                                vid_path = save_path
+                                if isinstance(vid_writer, cv2.VideoWriter):
+                                    vid_writer.release()  # release previous video writer
+
+                                fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                                vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
+                            vid_writer.write(im0)
+                xml_file.write('</annotation>\n')
+        a+=1
+        print("progressing:",a)
+
+    # if save_txt or save_img:
+    #     print('Results saved to %s' % os.getcwd() + os.sep + out)
+    #     if platform == 'darwin':  # MacOS
+    #         os.system('open ' + save_path)
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+    print("----------done---------- \n")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, default='weights/best.pt', help='model.pt path')
+    parser.add_argument('--source', type=str, default='inference/img1', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='inference/output1', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.5, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    opt = parser.parse_args()
+    print(opt)
+    
+    with torch.no_grad():   
+        detect()
