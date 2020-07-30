f86f9341454e8867fe968aea8e583098bb430b9e
china56321 <42080579+china56321@users.noreply.github.com>
2020-07-30 16:58:01 +0800

    Add files via upload
    该脚本是用来加载ONNX模型检测图片，验证转换的ONNX模型是否正确。基于pytorch-YOLOv4

---
+++ demo_onnx_yolov4_darknet2onnx.py
@@ -0,0 +1,51 @@
+import sys
+import onnx
+import os
+import argparse
+import numpy as np
+import cv2
+import onnxruntime
+
+from tool.utils import *
+
+def detect():
+    onnx_path="/home/ambavm/make/pytorch-YOLOv4/yolov5s.onnx"
+    image_path="/home/ambavm/make/pytorch-YOLOv4/img/61_cell.jpg"
+    session = onnxruntime.InferenceSession(onnx_path)
+    # session = onnx.load(onnx_path)
+    image_src = cv2.imread(image_path)
+
+    # session = onnx.load(onnx_path)
+    IN_IMAGE_H = session.get_inputs()[0].shape[2]
+    IN_IMAGE_W = session.get_inputs()[0].shape[3]
+
+    # Input
+    resized = cv2.resize(image_src, (IN_IMAGE_W, IN_IMAGE_H), interpolation=cv2.INTER_LINEAR)
+    img_in = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+    img_in = np.transpose(img_in, (2, 0, 1)).astype(np.float32)
+    img_in = np.expand_dims(img_in, axis=0)
+    img_in /= 255.0
+    print("Shape of the network input: ", img_in.shape)
+
+    # Compute
+    input_name = session.get_inputs()[0].name
+
+    outputs = session.run(None, {input_name: img_in})
+
+    boxes = post_processing(img_in, 0.4, 0.6, outputs)
+
+    num_classes = 80
+    if num_classes == 20:
+        namesfile = 'data/voc.names'
+    elif num_classes == 80:
+        namesfile = 'data/coco.names'
+    else:
+        namesfile = 'data/names'
+
+    class_names = load_class_names(namesfile)
+    plot_boxes_cv2(image_src, boxes[0], savename='predictions_onnx.jpg', class_names=class_names)
+
+
+
+if __name__ == '__main__':
+    detect()
